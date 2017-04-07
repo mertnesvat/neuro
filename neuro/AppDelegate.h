e9ea2e2649e16f3d2bce94224b0ae393b64d2da7
Mert <mertnesvat@gmail.com>
2017-04-07 20:33:47 +0300

    Initial Commit

---
+++ neuro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  neuro
+//
+//  Created by Mert Neşvat on 07/04/2017.
+//  Copyright © 2017 Mert Neşvat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ neuro/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  neuro
+//
+//  Created by Mert Neşvat on 07/04/2017.
+//  Copyright © 2017 Mert Neşvat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
