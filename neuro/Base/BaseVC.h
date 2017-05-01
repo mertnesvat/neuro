d015c44b16d5b616014855721c8c97c2cbc6e94c
mert <mertnesvat@gmail.com>
2017-05-01 22:02:16 +0300

    Detail is WIP

---
+++ neuro/Base/BaseVC.h
@@ -0,0 +1,13 @@
+//
+//  BaseVC.h
+//  neuro
+//
+//  Created by Mert Neşvat on 01/05/2017.
+//  Copyright © 2017 Mert Neşvat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseVC : UIViewController
+
+@end
---
+++ neuro/VC/ObserveViewController.h
@@ -0,0 +1,13 @@
+//
+//  ObserveViewController.h
+//  neuro
+//
+//  Created by Mert Neşvat on 01/05/2017.
+//  Copyright © 2017 Mert Neşvat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ObserveViewController : UIViewController
+
+@end
