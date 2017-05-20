74dc25cf2b5283bf61cb12d7afbfc30609be4ef4
mert <mertnesvat@gmail.com>
2017-05-20 20:15:48 +0300

    models are integrated

---
+++ neuro/NSNumber+Number.h
@@ -0,0 +1,13 @@
+//
+//  NSNumber+Number.h
+//  neuro
+//
+//  Created by Mert Neşvat on 20/05/2017.
+//  Copyright © 2017 Mert Neşvat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNumber (Number)
+
+@end
---
+++ neuro/NSNumber+Number.m
@@ -0,0 +1,13 @@
+//
+//  NSNumber+Number.m
+//  neuro
+//
+//  Created by Mert Neşvat on 20/05/2017.
+//  Copyright © 2017 Mert Neşvat. All rights reserved.
+//
+
+#import "NSNumber+Number.h"
+
+@implementation NSNumber (Number)
+
+@end
